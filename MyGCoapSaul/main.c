5f6b24486c0aac1d2a6f892b679995d29accb77a
Soltani, Mohammad Fazel <mohammadfazel.soltani@haw-hamburg.de>
2022-05-11 11:23:26 +0200

    Create MyGCoapSaul Folder with Makefile and main-file (WIP)

---
+++ MyGCoapSaul/main.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2022 HAW Hamburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       Example for demonstrating GCOAP, SAUL and the SAUL registry
+ *
+ * @author      Frank Matthiesen and M. Fazel Soltani
+ *
+ * @}
+ */
+
+#include <stdio.h>
+
+#include "shell.h"
+#include "gcoap_example.h"
+
+int main(void)
+{
+    puts("Welcome to MyGCoapASaul Example!\n");
+    puts("Type `help` for help\n");
+}
