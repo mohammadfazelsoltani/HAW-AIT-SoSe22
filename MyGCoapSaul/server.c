1ef892092c3209e434d029220af2d12e0d94068a
Frank Matthiesen <herrhackmack@gmail.com>
2022-05-31 15:06:36 +0200

    wip

--- MyGCoapSaul/server.c
+++ MyGCoapSaul/server.c
@@ -80,18 +80,15 @@ static const coap_resource_t _resources[] = {
 };
 
 static ssize_t _led_handlerr(coap_pkt_t* pdu, uint8_t *buf, size_t len, void *ctx){
-    _led_handler(pdu, buf, len, ctx, 0);
-    return 0;
+    return _led_handler(pdu, buf, len, ctx, 0);
 }
 
 static ssize_t _led_handlerg(coap_pkt_t* pdu, uint8_t *buf, size_t len, void *ctx){
-    _led_handler(pdu, buf, len, ctx, 1);
-    return 0;
+    return _led_handler(pdu, buf, len, ctx, 1);
 }
 
 static ssize_t _led_handlerb(coap_pkt_t* pdu, uint8_t *buf, size_t len, void *ctx){
-    _led_handler(pdu, buf, len, ctx, 2);
-    return 0;
+    return _led_handler(pdu, buf, len, ctx, 2);
 }
 
 
