16559f6f79bc0c5ad4cf529384293cf23db12d03
Soltani, Mohammad Fazel <mohammadfazel.soltani@haw-hamburg.de>
2022-06-20 15:22:23 +0200

    wip

--- MyGCoapSaul/main.c
+++ MyGCoapSaul/main.c
@@ -104,8 +104,7 @@ int main(void)
 
     sprintf(regif, "[%s]", buffer);
     
-    puts("make_sock_ep");
-    puts(make_sock_ep(&remote,regif));
+    puts("make_sock_ep: %i", make_sock_ep(&remote,regif));
     //cord_ep_register(&remote,regif);
 
     puts("Registering with RD now, this may take a short while...");
