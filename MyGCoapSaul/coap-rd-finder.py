6d93793dfbff38fd682ef08874e6dae330c0fd36
Frank Matthiesen <herrhackmack@gmail.com>
2022-06-21 16:11:36 +0200

    wip

--- MyGCoapSaul/coap-rd-finder.py
+++ MyGCoapSaul/coap-rd-finder.py
@@ -8,8 +8,32 @@ import re
 
 from aiocoap import *
 
+import python_weather
+
+
+async def getweather():
+    # declare the client. format defaults to metric system (celcius, km/h, etc.)
+    client = python_weather.Client(format=python_weather.IMPERIAL)
+
+    # fetch a weather forecast from a city
+    weather = await client.find("Hamburg")
+
+    # returns the current day's forecast temperature (int)
+    print(weather.current.temperature)
+
+    # get the weather forecast for a few days
+    for forecast in weather.forecasts:
+        print(str(forecast.date), forecast.sky_text, forecast.temperature)
+
+    # close the wrapper once done
+    await client.close()
+
 
 async def main():
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(getweather())
+
     print("coap ressource directory finder started")
     if(len(sys.argv) != 2):
         print("please enter host address")
---
+++ MyGCoapSaul/coap-temperatur-monitor.py
@@ -0,0 +1,11 @@
+
+import asyncio
+
+import sys
+import json
+import time
+import re
+
+from aiocoap import *
+
+from requests import async
