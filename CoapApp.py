a3d57f283fcab5c8797ed3c0626064eb2131c227
Soltani, Mohammad Fazel <mohammadfazel.soltani@haw-hamburg.de>
2022-06-28 11:42:36 +0200

    bugfixed

--- CoapApp.py
+++ CoapApp.py
@@ -34,4 +34,12 @@ async def main():
 
 # main-Function
 if __name__ == "__main__":
-    asyncio.run(main())
+    # asyncio.run(main())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    task = loop.create_task(main())
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        print()
