a706f68e8693f3fb5f05dd2cbfe7d857a75f2adb
Soltani, Mohammad Fazel <mohammadfazel.soltani@haw-hamburg.de>
2022-06-29 10:03:05 +0200

    blue led on off

--- PythonApp/CoapClientApp.py
+++ PythonApp/CoapClientApp.py
@@ -7,16 +7,35 @@ from aiocoap import *
 
 logging.basicConfig(level=logging.INFO)
 
+async def get_current_accelerometer_status():
+    return Message(code=GET, uri='coap://[::1]/saul/mma8x5x/SENSE_ACCEL')
+
+async def get_led_blue():
+    return Message(code=GET, uri='coap://[::1]/saul/LED(blue)/ACT_SWITCH')
+
+async def set_led_blue(payload):
+    return Message(code=PUT, payload=payload, uri="coap://[::1]/saul/LED(blue)/ACT_SWITCH")
+
+async def get_led_green():
+    return Message(code=GET, uri='coap://[::1]/saul/LED(green)/ACT_SWITCH')
+
+async def set_led_green(payload):
+    return Message(code=PUT, payload=payload, uri="coap://[::1]/saul/LED(green)/ACT_SWITCH")
+
+async def get_led_red():
+    return Message(code=GET, uri='coap://[::1]/saul/LED(red)/ACT_SWITCH')
+
+async def set_led_red():
+    return Message(code=PUT, payload=payload, uri="coap://[::1]/saul/LED(red)/ACT_SWITCH")
+
 async def main():
     protocol = await Context.create_client_context()
     # PUT Example
     #await asyncio.sleep(2)
     # payload = b"The quick brown fox jumps over the lazy dog.\n" * 30
     # request = Message(code=PUT, payload=payload, uri="coap://localhost/other/block")
-    
     # GET Example
     request = Message(code=GET, uri='coap://[::1]/.well-known/core')
-
     try:
         response = await protocol.request(request).response
     except Exception as e:
@@ -27,6 +46,31 @@ async def main():
         resources = response.payload.decode("UTF-8").split(",")
         for resource in resources:
             print(resource)
+    ###
+    await asyncio.sleep(2)
+    protocol2 = await Context.create_client_context()
+    request2 = set_led_blue(1)
+    try:
+        response2 = await protocol2.request(request2).response
+    except Exception as e:
+        print('Failed to fetch resource:')
+        print(e)
+    else:
+        print('Result: %s\n%r'%(response.code, response.payload))
+        #resources2 = response2.payload.decode("UTF-8").split(",")
+        #for resource2 in resources2:
+        #    print(resource2)
+    ###
+    await asyncio.sleep(2)
+    protocol3 = await Context.create_client_context()
+    request3 = set_led_blue(0)
+    try:
+        response2 = await protocol3.request(request3).response
+    except Exception as e:
+        print('Failed to fetch resource:')
+        print(e)
+    else:
+        print('Result: %s\n%r'%(response.code, response.payload))
 
 # main-Function
 if __name__ == "__main__":
