63d45fe475386926c6fbe6e15146432d929b69a0
Soltani, Mohammad Fazel <mohammadfazel.soltani@haw-hamburg.de>
2022-06-29 10:22:07 +0200

    ask current accel status

--- PythonApp/CoapClientApp.py
+++ PythonApp/CoapClientApp.py
@@ -7,8 +7,17 @@ from aiocoap import *
 
 logging.basicConfig(level=logging.INFO)
 
-def get_current_accelerometer_status():
-    return Message(code=GET, uri='coap://[::1]/saul/mma8x5x/SENSE_ACCEL')
+async def get_current_accelerometer_status():
+    protocol = await Context.create_client_context()
+    request = Message(code=GET, uri='coap://[::1]/saul/mma8x5x/SENSE_ACCEL')
+    try:
+        response = await protocol.request(request).response
+    except Exception as e:
+        print('Failed to fetch resource:')
+        print(e)
+    else:
+        print('Result: %s\n%r'%(response.code, response.payload))
+
 
 def get_led_blue():
     return Message(code=GET, uri='coap://[::1]/saul/LED(blue)/ACT_SWITCH')
@@ -29,14 +38,16 @@ def set_led_red():
     return Message(code=PUT, payload=payload, uri="coap://[::1]/saul/LED(red)/ACT_SWITCH")
 
 async def main():
+    
+    get_current_accelerometer_status()
+
     protocol = await Context.create_client_context()
     # PUT Example
     #await asyncio.sleep(2)
     # payload = b"The quick brown fox jumps over the lazy dog.\n" * 30
     # request = Message(code=PUT, payload=payload, uri="coap://localhost/other/block")
     # GET Example
-    # request = Message(code=GET, uri='coap://[::1]/.well-known/core')
-    request = get_led_blue()
+    request = Message(code=GET, uri='coap://[::1]/.well-known/core')
     try:
         response = await protocol.request(request).response
     except Exception as e:
@@ -44,9 +55,9 @@ async def main():
         print(e)
     else:
         print('Result: %s\n%r'%(response.code, response.payload))
-        #resources = response.payload.decode("UTF-8").split(",")
-        #for resource in resources:
-        #    print(resource)
+        resources = response.payload.decode("UTF-8").split(",")
+        for resource in resources:
+            print(resource)
 
 # main-Function
 if __name__ == "__main__":
