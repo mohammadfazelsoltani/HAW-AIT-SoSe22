f430028140de2f137c4a743df95e1dd12c794d98
Soltani, Mohammad Fazel <mohammadfazel.soltani@haw-hamburg.de>
2022-06-29 10:33:15 +0200

    ask current accel status

--- PythonApp/CoapClientApp.py
+++ PythonApp/CoapClientApp.py
@@ -7,47 +7,37 @@ from aiocoap import *
 
 logging.basicConfig(level=logging.INFO)
 
-async def get_current_accelerometer_status():
-    protocol = await Context.create_client_context()
-    request = Message(code=GET, uri='coap://[::1]/saul/mma8x5x/SENSE_ACCEL')
-    try:
-        response = await protocol.request(request).response
-    except Exception as e:
-        print('Failed to fetch resource:')
-        print(e)
-    else:
-        print('Result: %s\n%r'%(response.code, response.payload))
-
+def get_current_accelerometer_status():
+    return Message(code=GET, uri='coap://[2001:67c:254:b0b2:affe:3060:6eff:7da6]/saul/mma8x5x/SENSE_ACCEL')
 
 def get_led_blue():
-    return Message(code=GET, uri='coap://[::1]/saul/LED(blue)/ACT_SWITCH')
+    return Message(code=GET, uri='coap://[2001:67c:254:b0b2:affe:3060:6eff:7da6]/saul/LED(blue)/ACT_SWITCH')
 
 def set_led_blue(payload):
-    return Message(code=PUT, payload=payload, uri="coap://[::1]/saul/LED(blue)/ACT_SWITCH")
+    return Message(code=PUT, payload=payload, uri="coap://[2001:67c:254:b0b2:affe:3060:6eff:7da6]/saul/LED(blue)/ACT_SWITCH")
 
 def get_led_green():
-    return Message(code=GET, uri='coap://[::1]/saul/LED(green)/ACT_SWITCH')
+    return Message(code=GET, uri='coap://[2001:67c:254:b0b2:affe:3060:6eff:7da6]/saul/LED(green)/ACT_SWITCH')
 
 def set_led_green(payload):
-    return Message(code=PUT, payload=payload, uri="coap://[::1]/saul/LED(green)/ACT_SWITCH")
+    return Message(code=PUT, payload=payload, uri="coap://[2001:67c:254:b0b2:affe:3060:6eff:7da6]/saul/LED(green)/ACT_SWITCH")
 
 def get_led_red():
-    return Message(code=GET, uri='coap://[::1]/saul/LED(red)/ACT_SWITCH')
+    return Message(code=GET, uri='coap://[2001:67c:254:b0b2:affe:3060:6eff:7da6]/saul/LED(red)/ACT_SWITCH')
 
 def set_led_red():
-    return Message(code=PUT, payload=payload, uri="coap://[::1]/saul/LED(red)/ACT_SWITCH")
+    return Message(code=PUT, payload=payload, uri="coap://[2001:67c:254:b0b2:affe:3060:6eff:7da6]/saul/LED(red)/ACT_SWITCH")
 
 async def main():
     
-    get_current_accelerometer_status()
-
     protocol = await Context.create_client_context()
     # PUT Example
     #await asyncio.sleep(2)
     # payload = b"The quick brown fox jumps over the lazy dog.\n" * 30
     # request = Message(code=PUT, payload=payload, uri="coap://localhost/other/block")
     # GET Example
-    request = Message(code=GET, uri='coap://[::1]/.well-known/core')
+    # request = Message(code=GET, uri='coap://[::1]/.well-known/core')
+    request = get_current_accelerometer_status()
     try:
         response = await protocol.request(request).response
     except Exception as e:
